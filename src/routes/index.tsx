079da50257cd58e938f96e80e34ee77c7cd852f2
Vasco Pinto <vasco20gouveia259@outlook.com>
2022-04-13 13:51:22 +0100

    Configuração: Activando o dark mode.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,16 @@
 import { Button } from '@mui/material';
 
 import{Routes, Route, Navigate} from 'react-router-dom';
+import { useAppThemeContext } from '../pages';
 
 export const  AppRoutes =() =>{
+  const { toggleTheme} = useAppThemeContext ();
+
     return (
   
       <Routes> 
             
-            <Route path="/pagina-inicial" element={ <Button variant='contained' color='primary'>Página inicial</Button>} /> 
+            <Route path="/pagina-inicial" element={ <Button variant='contained' color='primary' onClick={ toggleTheme}>Alterar o fundo</Button>} /> 
             <Route path="*" element={<Navigate to="/pagina-inicial" />} />  
             
       </Routes> 
---
+++ src/shared/contexts/ThemeContext.tsx
@@ -0,0 +1,44 @@
+import { createContext, useCallback, useContext, useMemo, useState } from "react";
+import {ThemeProvider, Box} from "@mui/material";
+import { LightTheme, DarkTheme } from "./../themes";
+
+
+interface IThemeContexData  {
+    themeName: 'light' | 'dark';
+    toggleTheme: ()=> void;
+}
+
+const ThemeContext = createContext({} as IThemeContexData);
+
+export const useAppThemeContext = () =>{
+
+    return useContext (ThemeContext);
+}
+
+export const AppThemeProvider: React.FC = ({children}) => {
+    const [themeName, setThemeName] = useState <'light' | 'dark'> ('light');
+    
+    const toggleTheme=useCallback(() =>{
+        setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light');
+    }, []);
+
+    const theme = useMemo (()=> {
+        if (themeName === 'light') return LightTheme;
+        return DarkTheme;
+    }, [themeName])
+
+
+
+    return(
+        <ThemeContext.Provider value={{themeName, toggleTheme}}>
+             <ThemeProvider theme={theme} >
+            <Box width="100vw" height="100vh" bgcolor={theme.palette.background.default}>
+            {children}
+            </Box>
+            </ThemeProvider >
+        </ThemeContext.Provider>
+
+
+    
+    );
+}
